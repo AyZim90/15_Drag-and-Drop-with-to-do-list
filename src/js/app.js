89a3facc527bb8e9186ab094f2c427c82b12ae35
AyZim90 <ayanzimina@gmail.com>
2023-10-10 16:29:26 +0400

    update: added Fetch

--- src/js/app.js
+++ src/js/app.js
@@ -71,3 +71,28 @@ function addNewTask() {
         newTask.addEventListener('dragend', dragend);
     }
 }
+// Fetch код учителя
+
+let btn=document.getElementById("fetch");
+
+btn.addEventListener("click",function(){
+fetch("https://randomuser.me/api/?results=100")
+.then((response)=>response.json())
+.then((data)=>{
+    // console.log(data);
+    console.log(data.results);
+    let users=data.results;
+    let output="";
+    for(let user of users){
+        output+=`
+        <div class="card">
+        <img src="${user.picture.large}" alt="">
+        <h3>${user.name.title} ${user.name.first} ${user.name.last}</h3>
+        <p>${user.email}</p>
+        <p>${user.location.city}</p>
+        </div>
+        `
+    }
+    document.getElementById("output").innerHTML=output;
+})
+});
